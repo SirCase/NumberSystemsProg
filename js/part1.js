435d7e42db536a6b9a052dcecbc37669c4d0a801
Justin <jansell1@hwemail.com>
2020-01-21 00:59:00 -0800

    Redid Parts 1 and 2
    Redid Parts 1 and 2 because apparently using bitwise shifts is cheating

--- js/part1.js
+++ js/part1.js
@@ -1,10 +1,78 @@
 function ConversionPart1() {
 
-    var UnsignedInt = parseInt(document.getElementById("1_UnsignedInt").value);
+    var UnsignedInt = "" + document.getElementById("1_UnsignedInt").value;
     var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
     var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
 
-    var outputValue = parseInt(UnsignedInt, UnsignedIntBaseFrom).toString(UnsignedIntBaseTo);
+    var digits = [];
 
+    for (var i = 0; i < UnsignedInt.length; i++){
+      var digit = UnsignedInt.charAt(i);
+      digits.push(fixAboveNine(digit));
+    }
+    var baseTenValue = 0;
+    for (var i = 0; i < digits.length; i++){
+      baseTenValue = baseTenValue * UnsignedIntBaseFrom + digits[i];
+    }
+    var output = [];
+    while (baseTenValue != 0){
+      output.unshift(baseTenValue % UnsignedIntBaseTo);
+      baseTenValue = Math.floor(baseTenValue / UnsignedIntBaseTo);
+    }
+    var outputValue = "";
+    for (var i = 0; i < output.length; i++){
+      outputValue += undoAboveNine(output[i]);
+    }
+    outputValue= outputValue==""? 0: outputValue;
   FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
+}
+function fixAboveNine(digit){
+  switch (digit) {
+    case 'A':
+      return 10;
+      break;
+    case 'B':
+      return 11;
+      break;
+    case 'C':
+      return 12;
+      break;
+    case 'D':
+      return 13;
+      break;
+    case 'E':
+      return 14;
+      break;
+    case 'F':
+      return 15;
+      break;
+    default:
+      return parseInt(digit);
+      break;
+  }
+}
+function undoAboveNine(digit){
+  switch (digit) {
+    case 10:
+      return 'A';
+      break;
+    case 11:
+      return 'B';
+      break;
+    case 12:
+      return 'C';
+      break;
+    case 13:
+      return 'D';
+      break;
+    case 14:
+      return 'E';
+      break;
+    case 15:
+      return 'F';
+      break;
+    default:
+      return parseInt(digit);
+      break;
+  }
 }
--- js/part2.js
+++ js/part2.js
@@ -1,9 +1,47 @@
 function ConversionPart2() {
-    //
-    var SignedDecimalInt = parseInt(document.getElementById("2_SignedInt").value);
 
-    var value = "000000000000000000000000" + (SignedDecimalInt >>> 0).toString(2);
-    var twos = "000000000000000000000000" + (-SignedDecimalInt >>> 0).toString(2);
+    var SignedDecimalInt = document.getElementById("2_SignedInt").value;
+    var neg = SignedDecimalInt >=0? false: true;
 
-    FormatAndShowOutput([space(value.substring(value.length-24)), space(twos.substring(twos.length-24)), SignedDecimalInt], 2);
+    for (var i = 0; i < SignedDecimalInt.length; i++){
+      var digit = SignedDecimalInt.charAt(i);
+    }
+    var baseTwoValue = [];
+    var baseTenValue = Math.abs(SignedDecimalInt);
+    while (baseTenValue != 0){
+      baseTwoValue.unshift(baseTenValue % 2);
+      baseTenValue = Math.floor(baseTenValue / 2);
+    }
+    var value = "";
+    var twos = "";
+      for (var i = 0; i < baseTwoValue.length; i++){
+        value += baseTwoValue[i];
+      }
+      value = 0 + value;
+      baseTwoValue = flipVals(baseTwoValue);
+      for (var i = 0; i < baseTwoValue.length; i++){
+        twos += baseTwoValue[i];
+      }
+      twos = (SignedDecimalInt == 0? 0:1 )+ twos;
+      if (neg){
+        var temp = twos;
+        twos = value;
+        value = temp;
+      }
+
+    FormatAndShowOutput([space(value), space(twos), SignedDecimalInt], 2);
+}
+function flipVals(array){
+  for (var i = 0; i < array.length; i++){
+    array[i] = array[i] == 0? 1:0;
+  }
+  var a = array.length-1;
+  while(array[a] == 1){
+    a--;
+  }
+  array[a] = 1;
+  for (var i = a + a; i < array.length; i++){
+    array[i] = 0;
+  }
+  return array;
 }
